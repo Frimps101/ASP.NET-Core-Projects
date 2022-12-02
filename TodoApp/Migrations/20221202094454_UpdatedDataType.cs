6a198518aeabafead65172d7b808b4349e1bc92a
Josephine Frimpomaa kwakye <jkwakye001@gmail.com>
2022-12-02 06:01:22 -0800

    Adding to a repo

---
+++ TodoApp/Migrations/20221202094454_UpdatedDataType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Migrations
+{
+    public partial class UpdatedDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TodoTitle",
+                table: "Todo",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleDescription",
+                table: "Todo",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TodoTitle",
+                table: "Todo",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TitleDescription",
+                table: "Todo",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string TodoTitle { get; set; }
+        [Display(Name = "Description")]
+        public string TitleDescription { get; set; }
+    }
+}
